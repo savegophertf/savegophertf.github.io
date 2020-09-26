bba1eed54d46712355f054bee43a38469052ec02
yung-frodo <40253658+yung-frodo@users.noreply.github.com>
2020-09-25 19:25:59 -0700

    fix scrolly with header deleted

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,7 +99,7 @@
 		$('.smooth-scroll').scrolly();
 		$('.smooth-scroll-middle').scrolly({ anchor: 'middle' });
 		$('.smooth-scroll-top').scrolly({ anchor: 'top'});
-		$('.smooth-scroll-top-navbar-offset').scrolly({ anchor: 'top', offset: window.screen.height*.11})
+		$('.smooth-scroll-top-navbar-offset').scrolly({ anchor: 'top', offset: window.screen.height*.07})
 
 	// Wrapper.
 		$wrapper.children()
